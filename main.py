8ca46d51c65d1cca6d776cd9641dfd60c830caf0
Jonghwan Na <95013904+JohnSodium@users.noreply.github.com>
2023-02-06 17:08:31 +0900

    Update main.py
    Obtain feature vectors for the source and target nodes

--- main.py
+++ main.py
@@ -2,31 +2,63 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import count_lc
 from scipy.linalg import fractional_matrix_power
 
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
-for i in range(0, 1):
-    node_data = pd.read_csv(f"./adjacency_matrix/{i+400}_output.csv")
+lc_frame = count_lc.get_Swap_Frame()
+prev_frame = None
+node_data = None
 
+lc_frame = lc_frame[:10]
+for i in range(len(lc_frame)):
+    print(lc_frame[i])
+    feature_vector = pd.read_csv(f"./new_input_feature_matrix_20maxy/"
+                                 f"{lc_frame[i]}_features_with_label.csv")
     G = nx.Graph(name="G")
 
     vehicle_list = []
     edges = []
 
-    # Find connections between vehicles using the adjacency matrix
-    for index, row in node_data.iterrows():
+    source_features = []
+    target_features = []
 
-        for j in range(1, len(row.index)):
-            if j == 1:  # only add vehicle_id once
-                vehicle_list.append(row[0])
+    for j in range(len(feature_vector)):
+        print(j, '/', len(feature_vector))
+        cur_frame = int(feature_vector["Frame"][j])
+        if cur_frame != prev_frame:
+            node_data = pd.read_csv(f"./adjacency_matrix/{cur_frame}_output.csv")
 
-            # Create an edge when there is a connection between two vehicles
-            if row[j] == 1:
-                edges.append((int(row[0]), int(row.index[j])))
+            # Find connections between vehicles using the adjacency matrix
+            for index, row in node_data.iterrows():
+                for k in range(1, len(row.index)):
+                    if k == 1 and int(row[0]) not in vehicle_list:  # only add vehicle_id once
+                        vehicle_list.append(int(row[0]))
+                    # Create an edge when there is a connection between two vehicles
+                    if row[k] == 1:
+                        # print(int(row[0]), int(row.index[k]))
+                        edges.append((int(row[0]), int(row.index[k])))
 
+                        # Obtain features for the source node
+                        separate_by_id_source = feature_vector[feature_vector['Vehicle_ID'] == row[0]]
+                        separate_by_frame_source = separate_by_id_source[separate_by_id_source['Frame'] == cur_frame]
+
+                        source_features.append(separate_by_frame_source.values.tolist())
+
+                        # Obtain features for the target node
+                        separate_by_id_target = feature_vector[feature_vector['Vehicle_ID'] == int(row.index[k])]
+                        separate_by_frame_target = separate_by_id_target[separate_by_id_target['Frame'] == cur_frame]
+
+                        target_features.append(separate_by_frame_target.values.tolist())
+
+            # End of the iteration, previous frame becomes current frame
+            prev_frame = cur_frame
+
+    print(target_features)
+    print()
     # Create nodes based on the number of vehicles in the scene
     for j in range(len(vehicle_list)):
         G.add_node(vehicle_list[j], name=vehicle_list[j])
@@ -41,5 +73,6 @@ for i in range(0, 1):
     print('\nGraph Nodes: ', G.nodes.data())
 
     # Plot the graph
-    nx.draw(G, with_labels=True, font_weight='bold')
+    plt.figure(3, figsize=(12, 12))
+    nx.draw_circular(G, with_labels=True, font_weight='bold')
     plt.show()
