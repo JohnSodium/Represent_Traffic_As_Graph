963408336ae874c6c6d062839bd7fc66a9c0284f
Jonghwan Na <95013904+JohnSodium@users.noreply.github.com>
2023-02-06 17:11:44 +0900

    Create main.py

---
+++ create_feature_matrix/main.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+def flatten(input):
+    new_list = []
+    for i in input:
+        for j in i:
+            new_list.append(j)
+    return new_list
+
+
+sorted_file = pd.read_csv('sorted.csv')
+count_file = pd.read_csv('count.csv')
+counter = -1
+data = []
+frame = None
+
+for i in range(len(count_file)):
+    print(i, "/", len(count_file))
+    flag = -1  # flag to indicate when all vehicles with the same Frame_ID is found
+    for j in range(count_file["Occurrence"][i]):
+        counter += 1  # Counter to know which entry to look at in sorted_file
+        flag += 1
+        # print(j, sorted_file["Frame_ID"][counter], counter)
+        # Find the edge once all the vehicles with the same Frame_ID is found
+        frame = sorted_file["Frame_ID"][counter]
+        if flag == count_file["Occurrence"][i] - 1:
+            # Go back and find every vehicle with the same Frame_ID. Subtract flag each time until 0
+            for k in range(count_file["Occurrence"][i]):
+                # print(sorted_file["Vehicle_ID"][counter - flag])
+                data.append([
+                    sorted_file["Vehicle_ID"][counter - flag],
+                    sorted_file["Frame_ID"][counter - flag],
+                    sorted_file["Local_X"][counter - flag],
+                    sorted_file["Local_Y"][counter - flag],
+                    sorted_file["v_Vel"][counter - flag],
+                    sorted_file["v_Acc"][counter - flag],
+                    sorted_file["Lane_ID"][counter - flag]
+                ])
+
+                if flag != 0:
+                    flag -= 1
+            path = f'./input_feature_matrix/{frame}_feature_matrix_output.csv'
+            df = pd.DataFrame.from_records(data)
+            df.to_csv(path, index=False)
+            data = []
